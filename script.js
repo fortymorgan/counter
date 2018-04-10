f0e13b8db15c27771bd8fab0e881fbc552f21eb4
Forty Morgan <dimazhmudikov@gmail.com>
2018-04-10 15:33:18 +0300

    add button event listeners

---
+++ script.js
@@ -0,0 +1,2 @@
+document.getElementById('plus').addEventListener("click", e => console.log('click plus'))
+document.getElementById('minus').addEventListener("click", e => console.log('click minus'))
