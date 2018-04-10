cc1047b018813453a750184f61ded84787c01770
Forty Morgan <dimazhmudikov@gmail.com>
2018-04-10 15:56:49 +0300

    add counter with modifier

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-document.getElementById('plus').addEventListener("click", e => console.log('click plus'))
-document.getElementById('minus').addEventListener("click", e => console.log('click minus'))
+document.getElementById('plus').addEventListener("click", e => {
+    counter += 1;
+    document.getElementById('counter').innerHTML = `${counter}`
+})
+document.getElementById('minus').addEventListener("click", e => {
+    counter -= 1;
+    document.getElementById('counter').innerHTML = `${counter}`
+})
+
+let counter = 0;
