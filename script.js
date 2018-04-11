194104458be99468a3d5fb932a235c79a534389f
Forty Morgan <dimazhmudikov@gmail.com>
2018-04-11 13:44:32 +0300

    change render method

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.querySelector('#plus').addEventListener("click", e => {
     counter += 1;
-    document.querySelector('#counter').innerHTML = `${counter}`
+    document.querySelector('#counter').innerText = `${counter}`
 })
 document.querySelector('#minus').addEventListener("click", e => {
     counter -= 1;
-    document.querySelector('#counter').innerHTML = `${counter}`
+    document.querySelector('#counter').innerText = `${counter}`
 })
 
 let counter = 0;
